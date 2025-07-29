f384d47b384cc368990b9caebf7a3d4392188b27
Halil Eren Öymez <erenhalil2010@hotmail.com>
2025-07-29 13:16:57 +0300

    correct folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-
+import { Button } from "@/components/ui/button"
+ 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
@@ -11,7 +12,7 @@ export default function Home() {
           width={180}
           height={38}
           priority
-        />
+        />  
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
